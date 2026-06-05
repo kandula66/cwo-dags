6cbcd4f8458b981c2cb16a98956f455b2852b51e
kandula66 <kandula_rb@yahoo.com>
2026-06-05 10:42:39 -0500

    Create test_airflow_onprem.py

---
+++ test_airflow_onprem.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from airflow import DAG
+from cloudera.airflow.providers.operators.cde import CdeRunJobOperator
+
+with DAG(
+    dag_id="airflow_test_cde_onprem_dag",
+    schedule=None,   # manual trigger
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=["cde", "test"],
+) as dag:
+
+    run_airflow_test_job = CdeRunJobOperator(
+        task_id="run_airflow_test",
+        job_name="airflow_test",
+        connection_id="on-premcde"
+    )
